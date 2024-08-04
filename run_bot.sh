76694e2579d029808136a0f17fdf62166fac8b88
Sam Kolb <samuel.kolb@gmail.com>
2024-08-04 10:36:13 -0700

    making sure script enables venv

--- run_bot.sh
+++ run_bot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Paths to the Python script and log file
+VENV_PATH="/path/to/spamwaster-env"
 PYTHON_SCRIPT_PATH="spamwaster-telegram.py"
 LOG_FILE_PATH="output.log"
 
+# Check if the virtual environment is activated
+if [ -z "$VIRTUAL_ENV" ]; then
+    # Activate the virtual environment
+    source $VENV_PATH/bin/activate
+fi
 # Command to run the Python script in the background and redirect output to the log file
 nohup python3 $PYTHON_SCRIPT_PATH > $LOG_FILE_PATH 2>&1 &
 
