0fda70b66a5d07fbcd77f1bbcacf010065ad7def
Sam Kolb <samuel.kolb@gmail.com>
2024-08-12 11:11:52 -0700

    serve logs

--- run_bot.sh
+++ run_bot.sh
@@ -5,6 +5,7 @@ VENV_PATH="spamwaster"
 PYTHON_SCRIPT_PATH="spamwaster-telegram.py"
 PYTHON_FLASK_PATH="serve_logs"
 LOG_FILE_PATH="output.log"
+FLASK_LOG_FILE_PATH="flask.log"
 
 # Check if the virtual environment is activated
 if [ -z "$VIRTUAL_ENV" ]; then
@@ -13,7 +14,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
 fi
 # Command to run the Python script in the background and redirect output to the log file
 nohup python3 $PYTHON_SCRIPT_PATH > $LOG_FILE_PATH 2>&1 &
-nohup gunicorn -w 4 -b 0.0.0.0:8000 $PYTHON_FLASK_PATH:app > flask.log 2>&1 &
-
+nohup gunicorn -w 4 -b 0.0.0.0:8000 --timeout 120 $PYTHON_FLASK_PATH:app > $FLASK_LOG_FILE_PATH 2>&1 &
 
 echo "Python script is running and logging to $LOG_FILE_PATH"
+echo "Flask app is running and logging to $FLASK_LOG_FILE_PATH"
