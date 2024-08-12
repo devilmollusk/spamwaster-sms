c64729dbd07483d7cf5e9e0efe26de7b0d7eabce
Sam Kolb <samuel.kolb@gmail.com>
2024-08-12 10:32:48 -0700

    more script tweaks

--- run_bot.sh
+++ run_bot.sh
@@ -13,7 +13,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
 fi
 # Command to run the Python script in the background and redirect output to the log file
 nohup python3 $PYTHON_SCRIPT_PATH > $LOG_FILE_PATH 2>&1 &
-nohup gunicorn -w 4 -b 0.0.0.0:8000 app:$PYTHON_FLASK_PATH > flask.log 2>&1 &
+nohup gunicorn -w 4 -b 0.0.0.0:8000 $PYTHON_FLASK_PATH:$PYTHON_FLASK_PATH > flask.log 2>&1 &
 
 
 echo "Python script is running and logging to $LOG_FILE_PATH"
