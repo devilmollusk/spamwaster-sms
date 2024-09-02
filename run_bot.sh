0cc8797c8b17cd1b03113bf99c6d9efb7150f727
Sam Kolb <samuel.kolb@gmail.com>
2024-09-02 15:36:33 -0700

    fix script to just run bot

--- run_bot.sh
+++ run_bot.sh
@@ -12,9 +12,10 @@ if [ -z "$VIRTUAL_ENV" ]; then
     # Activate the virtual environment
     source $VENV_PATH/bin/activate
 fi
+
 # Command to run the Python script in the background and redirect output to the log file
-nohup python3 $PYTHON_SCRIPT_PATH > $LOG_FILE_PATH 2>&1 &
-# nohup gunicorn -w 1 -k eventlet -b 0.0.0.0:8000 $PYTHON_FLASK_PATH:app > $FLASK_LOG_FILE_PATH 2>&1 &
+nohup exec python3 -u $PYTHON_SCRIPT_PATH > $LOG_FILE_PATH 2>&1 &
+# nohup exec gunicorn -w 1 -k eventlet -b 0.0.0.0:8000 $PYTHON_FLASK_PATH:app > $FLASK_LOG_FILE_PATH 2>&1 &
 
 echo "Python script is running and logging to $LOG_FILE_PATH"
 #echo "Flask app is running and logging to $FLASK_LOG_FILE_PATH"
